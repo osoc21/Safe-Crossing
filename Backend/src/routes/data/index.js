dfaec67814f3764cf3eabe7e23675ef3a7df97b0
Andrews <andrewsleruth@gmail.com>
2021-07-07 16:03:40 +0200

    nodemon not working fixed and changed variable from file to image

--- Backend/src/routes/data/index.js
+++ Backend/src/routes/data/index.js
@@ -5,10 +5,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'python_tools/uploads')
+    cb(null, 'src/python_tools/uploads');
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   }
 })
 
@@ -20,8 +20,8 @@ module.exports = router
     res.send('This is the Data route in the API!');
   })
 
-  .post('/', upload.single('file'), (req, res, next) => {
-    // req.file is the `profile-file` file
+  .post('/', upload.single('image'), (req, res, next) => {
+    // req.file is the `image` file
     // req.body will hold the text fields, if there were any
 
     console.log(JSON.stringify(req.file));
