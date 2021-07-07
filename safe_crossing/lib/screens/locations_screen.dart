af7bcb1d825a989824b341dd352fbc539acd772d
Bolat Tleubayev <bolat.tleubayev@nu.edu.kz>
2021-07-07 10:23:45 +0200

    modified locations screen

--- safe_crossing/lib/screens/locations_screen.dart
+++ safe_crossing/lib/screens/locations_screen.dart
@@ -12,6 +12,8 @@ class LocationsScreen extends StatefulWidget {
 
 class _LocationsScreenState extends State<LocationsScreen> {
   int _counter = 0;
+  List<String> titles = ['Home', 'Work', 'Study'];
+  List<IconData> icons = [Icons.home, Icons.work, Icons.school];
 
   void _incrementCounter() {
     setState(() {
@@ -29,11 +31,29 @@ class _LocationsScreenState extends State<LocationsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: titles.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(icons[index]),
+                      title: Text('${titles[index]}'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ),
+                  );
+                },
+              ),
+            ),
             ElevatedButton(
-                child: Text('Say'),
-                onPressed: () {
-                  Navigator.pushNamed(context, MapScreen.routeName);
-                })
+              child: Text('Say'),
+              onPressed: () {
+                Navigator.pushNamed(context, MapScreen.routeName);
+              },
+            ),
+            SizedBox(
+              height: 30,
+            )
           ],
         ),
       ),
