6cda46ce5b0029bdec8c780f43388d4709490242
Mohammadreza Amini <m.ramini1999@gmail.com>
2021-07-12 13:40:14 +0200

    add of carousel view and some button redesign

--- safe_crossing/lib/screens/locations_screen.dart
+++ safe_crossing/lib/screens/locations_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'map_screen.dart';
 
@@ -11,15 +12,8 @@ class LocationsScreen extends StatefulWidget {
 }
 
 class _LocationsScreenState extends State<LocationsScreen> {
-  int _counter = 0;
-  List<String> titles = ['Home', 'Work', 'Study'];
+  List<String> locations = ['Home', 'Work', 'Study'];
   List<IconData> icons = [Icons.home, Icons.work, Icons.school];
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,58 +23,111 @@ class _LocationsScreenState extends State<LocationsScreen> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _createSizedBox(0, 30),
+            Expanded(child: _createLocationsCarousel()),
+            _createSizedBox(0, 30),
+            Column(
+              children: <Widget>[
+                _createAddLocationButton(),
+                _createSizedBox(0, 10),
+                _createMapButton(),
+              ],
+            ),
+            _createSizedBox(0, 30),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton _createMapButton() {
+    return ElevatedButton(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(10),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Icon(
+            Icons.map,
+            size: 25,
+          ),
+          _createSizedBox(10, 0),
+          Text(
+            'Map',
+            style: TextStyle(fontSize: 24),
+            textAlign: TextAlign.center,
+          ),
+        ]),
+      ),
+      onPressed: () {
+        Navigator.pushNamed(context, MapScreen.routeName);
+      },
+    );
+  }
+
+  ElevatedButton _createAddLocationButton() {
+    return ElevatedButton(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(10),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: titles.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Container(
-                      height: 120,
-                      child: Center(
-                        child: ListTile(
-                          leading: Icon(
-                            icons[index],
-                            size: 40,
-                          ),
-                          title: Text(
-                            '${titles[index]}',
-                            style: TextStyle(fontSize: 40),
-                          ),
-                          trailing: Icon(Icons.keyboard_arrow_right),
-                          onTap: () {
-                            Navigator.pushNamed(context, MapScreen.routeName);
-                          },
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+            Icon(
+              Icons.add_location,
+              size: 25,
             ),
-            ElevatedButton(
-              child: Container(
-                height: 300,
-                width: double.infinity,
-                child: Center(
-                  child: Text(
-                    'Voice',
-                    style: TextStyle(fontSize: 50),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, MapScreen.routeName);
-              },
-            ),
-            SizedBox(
-              height: 30,
+            Text(
+              'Add New Location',
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
             )
           ],
         ),
       ),
+      onPressed: () => print('Hello'),
+    );
+  }
+
+  SizedBox _createSizedBox(double width, double height) {
+    return SizedBox(
+      height: height,
+      width: width,
+    );
+  }
+
+  CarouselSlider _createLocationsCarousel() {
+    return CarouselSlider(
+      options: CarouselOptions(),
+      items: locations.map((location) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: BoxDecoration(color: Colors.indigo),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        icons[locations.indexOf(location)],
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      _createSizedBox(15, 0),
+                      Text(
+                        '$location',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontSize: 30.0),
+                      )
+                    ]));
+          },
+        );
+      }).toList(),
     );
   }
 }
