228b5adc7c83575fd83f1e05478c70b998d0be1e
Bolat Tleubayev <bolat.tleubayev@nu.edu.kz>
2021-07-15 09:29:56 +0200

    added osm screen + pin dropping

--- safe_crossing/lib/screens/osm_screen.dart
+++ safe_crossing/lib/screens/osm_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class OSMScreen extends StatefulWidget {
   static const routeName = '/osm-screen';
@@ -15,7 +16,29 @@ class _OSMScreenState extends State<OSMScreen> {
       appBar: AppBar(
         title: Text("OSM"),
       ),
-      body: Center(),
+      body: new FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(51.5, -0.09),
+          zoom: 13.0,
+        ),
+        layers: [
+          new TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c']),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 80.0,
+                height: 80.0,
+                point: new LatLng(51.5, -0.09),
+                builder: (ctx) => new Container(
+                  child: new FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
