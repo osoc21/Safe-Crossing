c747e7b46d82f5cc8497252f86c8c9c8c29736bf
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2021-07-26 15:49:27 +0200

    Github landing page

--- Backend/src/utils/socketio_client.js
+++
@@ -1,9 +0,0 @@
-// CommonJS
-const io = require("socket.io-client"),
-  ioClient = io.connect("http://localhost:3000");
-
-  ioClient.emit('run script');
-
-  // ioClient.emit('stop script');
-
-  ioClient.on("message", (data) => console.log(data));
--- Frontend/android/app/src/main/kotlin/be/osoc/safe_crossing/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package be.osoc.safe_crossing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
